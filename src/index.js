750d45e3a294f4cf823e36cd7a97fbddeeab7135
Gowrisankar24 <85389926+Gowrisankar24@users.noreply.github.com>
2022-03-26 15:25:28 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import UseMemoHook from './Hook_Concepts/UseMemoHook/UseMemoHook';
+//import ComponentA from './Hook_Concepts/UseReducer combined with UseContext/ComponentA';
+//import CounterOne from './Hook_Concepts/UseReducerHook/CounterOne';
+//import CompoA from './Hook_Concepts/UseContextHook/CompoA';
+import './index.css';
+//import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+      {/* <App />  */}
+     {/* <CompoA />  */}
+     {/* <CounterOne />  */}
+     {/* <ComponentA /> */}
+     {/* <UseMemoHook /> */}
+     <ParentComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
